ba8414ca3c0462edf6f74b84a947899662929a49
Dark-Elektron <55360766+Dark-Elektron@users.noreply.github.com>
2023-01-03 23:13:10 +0100

    Initial commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,7 +45,7 @@ extensions = [
     'sphinx.ext.inheritance_diagram',
     'matplotlib.sphinxext.mathmpl',
     'sphinx.ext.autosectionlabel',
-    'sphinxcontrib.bibtex'
+    # 'sphinxcontrib.bibtex'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
